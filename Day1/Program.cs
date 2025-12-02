369f453d778b6086d9f38861044afdbd699aeb1c
LumiInSpace <luminainspace@gmail.com>
2025-12-02 10:22:42 +0100

    Day 1 + Day 2

---
+++ Day1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.CompilerServices;
+using System.IO;
+
+namespace Day1
+{
+    public class Program
+    {
+        public readonly static string InputFile = "C:\\Dev\\AoC2025\\Day1\\input.txt";
+        public static Dictionary<int, int> NumberOfDigits { get; set; } = new Dictionary<int, int>();
+        public static int CurrentPosition { get; set; } = 50;
+
+        public static int Oversprings { get; set; } = 0;
+
+        static void Main(string[] args)
+        {
+            string[] fileContent = File.ReadAllLines(InputFile);
+            fileContent = fileContent
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToArray();
+
+            for (int i = 0; i < fileContent.Length; i++)
+            {
+                char[] chars = fileContent[i].ToCharArray();
+                char direction = chars[0];
+                int value = 0;
+
+                value = int.Parse(new string(chars, 1, chars.Length - 1));
+
+                if (direction == 'L')
+                {
+                    Oversprings += (int)(Math.Floor((decimal)CurrentPosition / value) - Math.Floor(((decimal)CurrentPosition - value) / 100));
+                    CurrentPosition -= value;
+                    CurrentPosition = (((CurrentPosition % 100) + 100) % 100);
+                }
+                else
+                {
+                    Oversprings += (int)(Math.Floor(((decimal)CurrentPosition + value) / 100) - Math.Floor(((decimal)CurrentPosition / 100)));
+                    CurrentPosition += value;
+                    CurrentPosition = (((CurrentPosition % 100) +100) % 100);
+                }
+
+
+                if (NumberOfDigits.ContainsKey(CurrentPosition) && NumberOfDigits.TryGetValue(CurrentPosition, out int currentValue))
+                {
+                    NumberOfDigits[CurrentPosition] = ++currentValue;
+                }
+                else
+                {
+                    NumberOfDigits.Add(CurrentPosition, 1);
+                }
+            }
+
+            Console.WriteLine(NumberOfDigits.Values.Max());
+            NumberOfDigits.TryGetValue(0, out int amount);
+            Console.WriteLine((Oversprings + amount).ToString());
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Day2
+{
+    internal class Program
+    {
+        public static string InputFilePath = "C:\\Dev\\AoC2025\\Day2\\input.txt";
+        public static long result = 0;
+
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(InputFilePath);
+            string[] ranges = input.Split(",");
+
+            foreach (var range in ranges)
+            {
+                var parts = range.Split('-');
+
+                for (long i = long.Parse(parts[0]); i < long.Parse(parts[1]); i++)
+                {
+                    int length = i.ToString().Length;
+                    List<int> dividers = new List<int>();
+                    for (int j = 1; j < length; j++)
+                    {
+                        if (length % j == 0)
+                        {
+                            dividers.Add(j);
+                        }
+                    }
+
+                    foreach (var divider in dividers)
+                    {
+                        List<int> blocks = new List<int>();
+                        Dictionary<int, int> comparisonTable = new Dictionary<int, int>();
+
+                        for (int j = 0; j < i.ToString().Length / divider; j++)
+                        {
+                            blocks.Add(int.Parse(i.ToString().Substring(j * divider, divider)));
+                        }
+
+                        for (int j = 0; j < blocks.Count; j++)
+                        {
+                            if (comparisonTable.ContainsKey(blocks[j])  == false) { comparisonTable[blocks[j]] = 1; }
+                            else {  comparisonTable[blocks[j]]++; }
+                        }
+
+                        if(comparisonTable.Keys.Count == 1 && comparisonTable.Values.First() >= 2)
+                        {
+                            result += i;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            Console.WriteLine(result);
+        }
+    }
+}
