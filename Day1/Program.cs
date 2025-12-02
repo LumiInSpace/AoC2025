19d5074a048b2759ad48a5af54e8067fa3fc6645
LumiInSpace <luminainspace@gmail.com>
2025-12-02 15:32:39 +0100

    Day 1 Part 2 fixed

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -6,53 +6,48 @@ namespace Day1
     public class Program
     {
         public readonly static string InputFile = "C:\\Dev\\AoC2025\\Day1\\input.txt";
-        public static Dictionary<int, int> NumberOfDigits { get; set; } = new Dictionary<int, int>();
         public static int CurrentPosition { get; set; } = 50;
-
-        public static int Oversprings { get; set; } = 0;
+        public static int LastPosition { get; set; } = 0;
+        public static int ZeroCount { get; set; } = 0;
 
         static void Main(string[] args)
         {
             string[] fileContent = File.ReadAllLines(InputFile);
-            fileContent = fileContent
-                .Where(x => !string.IsNullOrWhiteSpace(x))
-                .ToArray();
 
-            for (int i = 0; i < fileContent.Length; i++)
+            foreach (string line in fileContent)
             {
-                char[] chars = fileContent[i].ToCharArray();
+                LastPosition = CurrentPosition;
+                char[] chars = line.ToCharArray();
                 char direction = chars[0];
-                int value = 0;
+                int steps = int.Parse(line.Remove(0, 1));
 
-                value = int.Parse(new string(chars, 1, chars.Length - 1));
+                int rotations = steps / 100;
+                ZeroCount += rotations;
+                steps = steps % 100;
 
-                if (direction == 'L')
+                if (direction == 'R')
                 {
-                    Oversprings += (int)(Math.Floor((decimal)CurrentPosition / value) - Math.Floor(((decimal)CurrentPosition - value) / 100));
-                    CurrentPosition -= value;
-                    CurrentPosition = (((CurrentPosition % 100) + 100) % 100);
+                    CurrentPosition += steps;
                 }
                 else
                 {
-                    Oversprings += (int)(Math.Floor(((decimal)CurrentPosition + value) / 100) - Math.Floor(((decimal)CurrentPosition / 100)));
-                    CurrentPosition += value;
-                    CurrentPosition = (((CurrentPosition % 100) +100) % 100);
+                    CurrentPosition -= steps;
+                    if (CurrentPosition < 0)
+                    {
+                        CurrentPosition += 100;
+                    }
                 }
 
+                CurrentPosition %= 100;
 
-                if (NumberOfDigits.ContainsKey(CurrentPosition) && NumberOfDigits.TryGetValue(CurrentPosition, out int currentValue))
+
+                if (CurrentPosition == 0 || LastPosition != 0 && ((LastPosition > CurrentPosition && direction == 'R') || (LastPosition < CurrentPosition && direction == 'L')))
                 {
-                    NumberOfDigits[CurrentPosition] = ++currentValue;
-                }
-                else
-                {
-                    NumberOfDigits.Add(CurrentPosition, 1);
+                    ZeroCount++;
                 }
             }
 
-            Console.WriteLine(NumberOfDigits.Values.Max());
-            NumberOfDigits.TryGetValue(0, out int amount);
-            Console.WriteLine((Oversprings + amount).ToString());
+            Console.WriteLine(ZeroCount.ToString());
         }
     }
 }
